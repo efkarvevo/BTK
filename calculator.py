7b9a01a8d94c3889f3a9037aef8eec079cf985b0
efkarvevo <96696737+efkarvevo@users.noreply.github.com>
2022-12-05 15:17:00 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,77 +1,79 @@
 from tkinter import *
 from math import *
 
-def toplaiş():
-    s1=float(sayi1.get())
-    s2=float(sayi2.get())
-    sonuc.configure(text=str(s1+s2))
-def çıkartiş():
-    s1=float(sayi1.get())
-    s2=float(sayi2.get())
-    sonuc.configure(text=str(s1-s2))
-def çarpmaiş():
-    s1=float(sayi1.get())
-    s2=float(sayi2.get())
-    sonuc.configure(text=str(s1*s2))
-def bölmeiş():
-    s1=float(sayi1.get())
-    s2=float(sayi2.get())
-    sonuc.configure(text=str(s1/s2))
+def addjob():
+    s1=float(num1.get())
+    s2=float(num2.get())
+    equal.configure(text=str(s1+s2))
+def subtractjob():
+    s1=float(num1.get())
+    s2=float(num2.get())
+    equal.configure(text=str(s1-s2))
+def çarpmajob():
+    s1=float(num1.get())
+    s2=float(num2.get())
+    equal.configure(text=str(s1*s2))
+def dividejob():
+    s1=float(num1.get())
+    s2=float(num2.get())
+    equal.configure(text=str(s1/s2))
 def üsalma():
-    s1=float(sayi1.get())
-    s2=float(sayi2.get())
-    sonuc.configure(text=str(s1**s2))
+    s1=float(num1.get())
+    s2=float(num2.get())
+    equal.configure(text=str(s1**s2))
 def karakök():
-    s1=float(sayi1.get())
-    sonuc.configure(text=str(sqrt(s1)))
+    s1=float(num1.get())
+    equal.configure(text=str(sqrt(s1)))
 def sinal():
-    s1=float(sayi1.get())
-    sonuc.configure(text=str(sin(radians(s1))))
+    s1=float(num1.get())
+    equal.configure(text=str(sin(radians(s1))))
 def cosal():
-    s1=float(sayi1.get())
-    sonuc.configure(text=str(cos(radians(s1))))
+    s1=float(num1.get())
+    equal.configure(text=str(cos(radians(s1))))
 
 
 
-sayfa=Tk()
-sayfa.title("Hesap Makinesi")
-sayfa.geometry("327x276")
+page=Tk()
+page.title("Calculator")
+page.geometry("327x276")
+page.maxsize(327,276)
+page.minsize(327,276)
 
-sonuc=Label(sayfa,text="SONUÇ",font=("Bahnschrift SemiLight SemiConde",8),width=20,bg="#000080",fg="white")
-sonuc.grid(row=1,column=2)
+equal=Label(page,text="EQUAL",font=("Bahnschrift SemiLight SemiConde",8),width=20,bg="#000080",fg="white")
+equal.grid(row=1,column=2)
 
-sayi1=Entry(sayfa,font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#750d02",fg="#699ec3")
-sayi1.grid(row=2,column=2)
-sayi11=Label(sayfa,text="1.SAYI--->",font=("Bahnschrift SemiLight SemiConde",16))
-sayi11.grid(row=2,column=1)
+num1=Entry(page,font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#750d02",fg="#699ec3")
+num1.grid(row=2,column=2)
+num11=Label(page,text="1.num--->",font=("Bahnschrift SemiLight SemiConde",16))
+num11.grid(row=2,column=1)
 
-sayi2=Entry(sayfa,font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#750d02",fg="#699ec3")
-sayi2.grid(row=3,column=2)
-sayi22=Label(sayfa,text="2.SAYI--->",font=("Bahnschrift SemiLight SemiConde",16))
-sayi22.grid(row=3,column=1)
+num2=Entry(page,font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#750d02",fg="#699ec3")
+num2.grid(row=3,column=2)
+num22=Label(page,text="2.num--->",font=("Bahnschrift SemiLight SemiConde",16))
+num22.grid(row=3,column=1)
 
-topla=Button(sayfa,text="+",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=toplaiş,relief="groove")
-topla.grid(row=4,column=1)
+add=Button(page,text="+",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=addjob,relief="groove")
+add.grid(row=4,column=1)
 
-çıkart=Button(sayfa,text="-",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=çıkartiş,relief="groove")
-çıkart.grid(row=4,column=3)
+subtract=Button(page,text="-",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=subtractjob,relief="groove")
+subtract.grid(row=4,column=3)
 
-çarpma=Button(sayfa,text="x",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=çarpmaiş,relief="groove")
-çarpma.grid(row=5,column=1)
+multiply=Button(page,text="x",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=çarpmajob,relief="groove")
+multiply.grid(row=5,column=1)
 
-bölme=Button(sayfa,text="÷",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=bölmeiş,relief="groove")
-bölme.grid(row=5,column=3)
+divide=Button(page,text="÷",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=dividejob,relief="groove")
+divide.grid(row=5,column=3)
 
-üstalma=Button(sayfa,text="x^y",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=üsalma,relief="groove")
-üstalma.grid(row=6,column=1)
+square=Button(page,text="x^y",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=üsalma,relief="groove")
+square.grid(row=6,column=1)
 
-karakökal=Button(sayfa,text="√",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=karakök,relief="groove")
-karakökal.grid(row=6,column=3)
+squareroot=Button(page,text="√",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=karakök,relief="groove")
+squareroot.grid(row=6,column=3)
 
-sinüsal=Button(sayfa,text="sin(x)",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=sinal,relief="groove")
+sinüsal=Button(page,text="sin(x)",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=sinal,relief="groove")
 sinüsal.grid(row=7,column=1)
 
-cosinüsal=Button(sayfa,text="cos(x)",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=cosal,relief="groove")
+cosinüsal=Button(page,text="cos(x)",font=("Bahnschrift SemiLight SemiConde",16),width=10,bg="#587F40",command=cosal,relief="groove")
 cosinüsal.grid(row=7,column=3)
 
-sayfa.mainloop()
+page.mainloop()
